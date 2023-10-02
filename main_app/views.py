c5b23ecb6d9c1c3556ac1930e778b8d2351be303
barrientosjesus <jabarri1@gmail.com>
2023-10-02 16:31:46 -0700

    Update Vehicle model to include ImageField. Add AWS funtionality INCLUDING static file storage. Initial setup of Index page for vehicles.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from .models import Vehicle, Location
@@ -12,4 +13,17 @@ class Home(LoginView):
 class AddVehicle(CreateView):
     model = Vehicle
     fields = ['year', 'make', 'model', 'msrp', 'color', 'notes', 'engine', 'odometer', 'interior', 'transmission' , 'condition',
-    'vehicle_type', 'location']
+    'vehicle_type', 'location', 'image']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add Vehicle'
+        return context
+
+def vehicle_index(request):
+    vehicles = Vehicle.objects.all()
+
+    return render(request, 'vehicles/index.html', {
+        'vehicles': vehicles,
+        'title': 'Vehicle List'
+    })
